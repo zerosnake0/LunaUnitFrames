4d66156472e4ce8a36efeb3bb17353f656d9d053
zerosnake0 <zerosnake0@gmail.com>
2016-11-20 22:45:33 +0100

    [branch] new develop branch

--- LunaUnitFrames.lua
+++ LunaUnitFrames.lua
@@ -1,5 +1,6 @@
 LunaUF = AceLibrary("AceAddon-2.0"):new("AceEvent-2.0", "AceConsole-2.0", "AceDB-2.0", "AceHook-2.1", "FuBarPlugin-2.0", "AceDebug-2.0")
 LunaUF:RegisterDB("LunaDB")
+LunaUF:SetDebugging(true)
 
 -- Assets ----------------------------------------------------------------------------------
 LunaUF.Version = 2100
